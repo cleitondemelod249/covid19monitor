0acb0bf3d009a75cd0b49543fc0863cd7026db70
cleitondemelod249 <cleitondemelo@live.com>
2020-04-04 13:33:18 -0300

    Updates

---
+++ precache-manifest.e44dd60abe8d1020e9a9108f721202c2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d246a36fc637bb1c39b691d7837ba29",
+    "url": "/covid19-monitor/index.html"
+  },
+  {
+    "revision": "93a06cd9bf8b9cb3ff16",
+    "url": "/covid19-monitor/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5c3ac3b763f91f6247e3",
+    "url": "/covid19-monitor/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "93a06cd9bf8b9cb3ff16",
+    "url": "/covid19-monitor/static/js/2.3a7ae9b0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/covid19-monitor/static/js/2.3a7ae9b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c3ac3b763f91f6247e3",
+    "url": "/covid19-monitor/static/js/main.9c2081c2.chunk.js"
+  },
+  {
+    "revision": "011ea5d855a7077ee376",
+    "url": "/covid19-monitor/static/js/runtime-main.4d7dfcce.js"
+  },
+  {
+    "revision": "a88a7c23e89b731dbc33422f0b6a8a82",
+    "url": "/covid19-monitor/static/media/virus_PNG22.a88a7c23.png"
+  }
+]);
